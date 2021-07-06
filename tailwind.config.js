f434d779487eaa08fee7b00be33bcf67c0c9f85f
Willem Leuverink <willem@leuver.ink>
2021-07-06 20:42:20 +0200

    basic layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const colors = require('tailwindcss/colors')
 module.exports = {
     mode: 'jit',
     purge: [
-        './public/**/*.html',
+        './public/**/*.{html,js}',
     ],
     darkMode: false, // or 'media' or 'class'
     theme: {
