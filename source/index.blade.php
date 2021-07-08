4cc025389689f2285a99f88ad2e1c33098e83037
Willem Leuverink <willem@leuver.ink>
2021-07-08 19:56:08 +0200

    extract article list item

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,33 +1,8 @@
 
 <x-layout>
 
-    <article> <!-- BLOG ARTICLE -->
 
-        <header class="mb-6">
-            <a href="#">
-
-                <h2 class="max-w-lg text-2xl md:text-3xl font-bold leading-tight mb-1">
-                    A Laravel package to dispatch jobs via Artisan
-                </h2>
-
-                <p class="text-lg text-gray-700">
-                    <time datetime="2021-06-14T17:36:12+02:00">Jun 14th 2021</time> – 3 minute read
-                </p>
-
-            </a>
-        </header>
-
-        <div class="prose prose-xl leading-relaxed">
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate voluptas repudiandae omnis suscipit fuga asperiores reprehenderit sit adipisci autem nulla, voluptates alias nihil magnam expedita facere voluptatum accusantium dolorem id!
-            </p>
-
-            <a href="#" class="mt-6">
-                Read more
-            </a>
-        </div>
-
-    </article> <!-- END BLOG ARTICLE -->
+    <x-article-excerpt />
 
     <!-- SEPARATOR -->
     <div class="flex justify-center md:justify-start my-10">
@@ -45,5 +20,6 @@
 
     </div>
     <!-- END SEPARATOR -->
+
 
 </x-layout>
