80f6496cb5fb07ab2f0681c5ec1ffff914177e15
Willem Leuverink <willem@leuver.ink>
2021-08-05 19:35:26 +0200

    add rss feed

--- config.php
+++ config.php
@@ -7,6 +7,7 @@ return [
     'baseUrl' => 'http://localhost:3000/',
     'title' => 'Project X',
     'description' => 'Website description.',
+    'siteAuthor' => 'Willem Leuverink',
 
     'collections' => [
         'posts' => [
@@ -16,5 +17,33 @@ return [
 
     'selected' => function ($page, $section) {
         return $page->getPath() === $section || Str::contains($page->getPath(), $section) ? 'font-bold text-black' : '';
+    },
+
+    'getExcerpt' => function ($page, $length = 255) {
+        if ($page->excerpt) {
+            return $page->excerpt;
+        }
+
+        $content = preg_split('/<!-- more -->/m', $page->getContent(), 2);
+        $cleaned = trim(
+            strip_tags(
+                preg_replace(['/<pre>[\w\W]*?<\/pre>/', '/<h\d>[\w\W]*?<\/h\d>/'], '', $content[0]),
+                '<code>'
+            )
+        );
+
+        if (count($content) > 1) {
+            return $cleaned;
+        }
+
+        $truncated = substr($cleaned, 0, $length);
+
+        if (substr_count($truncated, '<code>') > substr_count($truncated, '</code>')) {
+            $truncated .= '</code>';
+        }
+
+        return strlen($cleaned) > $length
+            ? preg_replace('/\s+?(\S+)?$/', '', $truncated) . '...'
+            : $cleaned;
     },
 ];
---
+++ source/_components/article-rss.blade.php
@@ -0,0 +1,16 @@
+<entry>
+    <id>{{ $article->getUrl() }}</id>
+    <link type="text/html" rel="alternate" href="{{ $article->getUrl() }}" />
+    <title>{{ $article->title }}</title>
+    <published>{{ date(DATE_ATOM, $article->date) }}</published>
+    <updated>{{ date(DATE_ATOM, $article->date) }}</updated>
+    <author>
+        <name>{{ $article->author }}</name>
+    </author>
+    <summary type="html">{{ $article->getExcerpt() }}</summary>
+    <content type="html">
+    <![CDATA[
+        @includeFirst(['_posts.' . $article->getFilename(), '_posts._tmp.' . $article->getFilename()])
+    ]]>
+    </content>
+</entry>
