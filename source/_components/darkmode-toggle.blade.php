64b03be3fbf1bab7333ed3b3be9411811ee5ec30
Willem Leuverink <willem@leuver.ink>
2023-02-15 15:29:41 +0100

    boyscouting

--- source/_components/darkmode-toggle.blade.php
+++ source/_components/darkmode-toggle.blade.php
@@ -22,7 +22,7 @@
     <svg
         x-show="$store.darkMode.enabled"
         x-transition
-        class="absolute w-4 h-4 mt-0.5 right-1.5"
+        class="absolute w-4 h-4 mt-0.5 right-1.5 text-slate-300"
         xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
         <path fill-rule="evenodd" d="M7.455 2.004a.75.75 0 01.26.77 7 7 0 009.958 7.967.75.75 0 011.067.853A8.5 8.5 0 116.647 1.921a.75.75 0 01.808.083z" clip-rule="evenodd" />
     </svg>
