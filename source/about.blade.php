4bde37438396d0423166a0666af541595db386f5
Willem Leuverink <willem@leuver.ink>
2023-02-15 15:29:35 +0100

    add about page copy

--- source/about.blade.php
+++ source/about.blade.php
@@ -6,6 +6,54 @@ title: About
 
 @section('content')
 
-   Hello World!
+<div class="prose prose-xl">
+
+    <div class="flex flex-col items-center mb-8 space-y-6 sm:space-x-6 sm:flex-row">
+        <div class="inline-block overflow-hidden rounded-full shrink-0 grow-0 bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">
+           <img src="/assets/images/nature-boy-square-rect.jpg" class="pointer-events-none select-none m-1.5 rounded-full max-w-[180px] scale-x-[-1]" alt="That's me!">
+        </div>
+
+        <div>
+            <p>Before you you might see one of the great philosophers, staring into the distance, pondering the finer meaning of existence</p>
+            <p>Looks can be deceiving however, it is just me</p>
+        </div>
+    </div>
+
+    <p>
+        If you've got this far, I'd just like to say.. Hi mum, I know it's you. I've got my own website. Next time you ask me what it is I do, I'll just refer you back to this page.
+    </p>
+
+    <p>
+        I am a engineer by trade. Creative problem solving is what I do. Wherether that'd be by writing code or finding solutions that help businesses improve their efficiency.
+    </p>
+
+    <p>
+        A good program has much in common with written prose. It it is an expression of creativity. Clear in how it communicates it's intent and iterated over many times untill it is as perfect as can be.
+    </p>
+
+    <p>
+        A hallmark of good software in my opinion is not only that is well designed, written with the right abstractions & rigourosly tested. But it also has a certain astetic only an seasoned programmer might appreciate.
+        In that regard, I consider programming a highly creative endeavor. One that I pull a lot of satisfaction from.
+    </p>
+
+    <x-separator />
+
+    <p>
+        Having said all that, professional endeavours and interests are not what makes a man. Let's give you a short summary of some other things I enjoy, hopefully without boring you to a slumber.
+    </p>
+
+    <p>
+        I'm a proud husband and father to a (at the time of writing) newborn daughter. We recently moved house from the city to a quaint village, right next to national park Zuid Kennemerland.
+    </p>
+
+    <p>
+        Honestly, I was reluctand moving from the city at first but now can never go back. Living close to nature gives me a peace of mind I did not realize I was missing before.
+    </p>
+
+    <p>
+        When not busy taking care of our newborn I love going for a hike or a bike ride in the dunes or go for a quick surf in the sunset. I thoroughly enjoy getting my hands dirty wile pruning the garden or working on yet another DIY project.
+    </p>
+
+</div>
 
 @endsection
