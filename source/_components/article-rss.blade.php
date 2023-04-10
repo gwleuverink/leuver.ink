9e0a0ffc78e15d92618e60dc80a6c952c2b6af2f
Willem Leuverink <willem@leuver.ink>
2023-04-10 14:28:12 +0200

    fix xml feed markup

--- source/_components/article-rss.blade.php
+++ source/_components/article-rss.blade.php
@@ -5,12 +5,14 @@
     <published>{{ date(DATE_ATOM, $article->date) }}</published>
     <updated>{{ date(DATE_ATOM, $article->date) }}</updated>
     <author>
-        <name>{{ $article->author }}</name>
+        <name>{{ $article->author ?? 'Willem Leuverink' }}</name>
     </author>
     <summary type="html">{{ $article->getExcerpt() }}</summary>
-    <content type="html">
-    <![CDATA[
-        @includeFirst(['_posts.' . $article->getFilename(), '_posts._tmp.' . $article->getFilename()])
-    ]]>
-    </content>
+    {{--
+        <content type="html">
+        <![CDATA[
+            @includeFirst(['_posts.' . $article->getFilename(), '_posts._tmp.' . $article->getFilename()])
+        ]]>
+        </content>
+    --}}
 </entry>
