d0c67887d0577ec008344e18670ce94386eeb34f
Willem Leuverink <willem@leuver.ink>
2024-03-13 21:52:59 +0100

    homepage description & favicons

--- source/_layouts/main.blade.php
+++ source/_layouts/main.blade.php
@@ -16,6 +16,10 @@
     <link rel="manifest" href="/app.webmanifest">
     <link rel="canonical" href="{{ $page->getUrl() }}">
 
+    <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon/favicon-16x16.png">
+
     <!-- Google tag (gtag.js) -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-3V846Y2C35"></script>
     <script>
