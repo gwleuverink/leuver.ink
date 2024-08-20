a853d251a834a13e46eb79cc63606c77d5617ee1
gwleuverink <willem@leuver.ink>
2024-08-20 18:24:21 +0200

    improve darkmode input colors

--- source/_components/input/text.blade.php
+++ source/_components/input/text.blade.php
@@ -6,7 +6,7 @@
 <input
     {{ $attributes->merge([
         'type' => 'text',
-        'class' => 'dark:bg-slate-800 dark:text-white bg-transparent block w-full text-lg -skew-x-[4deg] transform-gpu border-slate-300 shadow-sm focus:border-pink-500 focus:ring-pink-500 focus:animate-pulse'
+        'class' => 'dark:bg-slate-800 dark:text-white dark:border-slate-700 bg-transparent block w-full text-lg -skew-x-[4deg] transform-gpu border-slate-300 shadow-sm focus:border-pink-500 focus:ring-pink-500 focus:animate-pulse'
     ]) }}
     :disabled="state.success || state.submitting || @js($disabled)"
     :id="$id('input')"
--- source/_components/input/textarea.blade.php
+++ source/_components/input/textarea.blade.php
@@ -6,7 +6,7 @@
 <textarea
     {{ $attributes->merge([
         'rows' => 5,
-        'class' => 'dark:bg-slate-800 dark:text-white bg-transparent block min-h-[4rem] max-h-[20rem] w-full text-lg -skew-x-[4deg] transform-gpu border-slate-300 shadow-sm focus:border-pink-500 focus:ring-pink-500 focus:animate-pulse'
+        'class' => 'dark:bg-slate-800 dark:text-white dark:border-slate-700 bg-transparent block min-h-[4rem] max-h-[20rem] w-full text-lg -skew-x-[4deg] transform-gpu border-slate-300 shadow-sm focus:border-pink-500 focus:ring-pink-500 focus:animate-pulse'
     ]) }}
     :disabled="state.success || state.submitting || @js($disabled)"
     :id="$id('input')"
