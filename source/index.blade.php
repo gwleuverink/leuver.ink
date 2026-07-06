fee177fa08b3bf4429e1f5518f281d8c81e51e5e
gwleuverink <willem@leuver.ink>
2026-07-06 13:15:38 +0200

    update bio

--- source/index.blade.php
+++ source/index.blade.php
@@ -13,13 +13,17 @@ pagination:
 <div class="prose prose-xl">
 
     <p role="heading" aria-level="2">
-        I'm <a href="https://twitter.com/gwleuverink" target="_blank">Willem</a>, Freelance at Trailhead Labs, <a href="https://nativephp.com" target="_blank">NativePHP</a> core team. Currently enamoured with
-        <a href="https://laravel.com" target="_blank">Laravel</a>, <a href="https://laravel-livewire.com" target="_blank">Livewire</a>, <a href="https://www.alpinejs.dev" target="_blank">AlpineJS</a> & <a href="https://tailwindcss.com" target="_blank">Tailwind</a>.
-        And let me be honest for a second, this thing has grown way past affection <x-svg.heart class="inline w-6 h-6 ml-1" />
+        I'm <a href="https://twitter.com/gwleuverink" target="_blank">Willem</a>, freelance engineer at <a href="https://trailheadlabs.co" target="_blank">Trailhead Labs</a> and <a href="https://nativephp.com" target="_blank">NativePHP</a> Desktop team.
+        Hand me a messy problem and I'll turn it into something that ships, works, and holds up. On the web, or off it.
     </p>
 
     <p>
-        So heck, you've found yourself here. Here's what you can expect: Some programming related <a href="{{ $page->baseUrl }}blog">articles</a>, Some highlighted <a href="{{ $page->baseUrl }}software">side projects</a> I work on. And how to <a href="{{ $page->baseUrl }}about">get in touch</a>.
+        If the years have taught me one thing, it's that the best software gets built shoulder to shoulder <x-svg.heart class="inline w-6 h-6 ml-1 mr-1.5" />
+        Bring me in as a partner, not a pair of hands, and you get someone who owns the outcome as much as you do.
+    </p>
+
+    <p>
+        When I'm not building for clients, I'm shipping <a href="{{ $page->baseUrl }}software">products of my own</a>. Around here you'll also find my <a href="{{ $page->baseUrl }}blog">writing</a> and how to <a href="{{ $page->baseUrl }}about">get in touch</a>.
     </p>
 
 
