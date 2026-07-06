273d05f1d515496815314a9e6dd6734251cb666e
gwleuverink <willem@leuver.ink>
2026-07-06 13:16:26 +0200

    update deps

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
 import jigsaw from "@tighten/jigsaw-vite-plugin";
 import { defineConfig } from "vite";
+import fs from "fs";
+import os from "os";
+import path from "path";
+
+const host = "leuver.ink.test";
+const certPath = path.resolve(
+  os.homedir(),
+  "Library/Application Support/Herd/config/valet/Certificates"
+);
 
 export default defineConfig({
+  server: {
+    host,
+    hmr: { host },
+    https: {
+      key: fs.readFileSync(path.resolve(certPath, `${host}.key`)),
+      cert: fs.readFileSync(path.resolve(certPath, `${host}.crt`)),
+    },
+  },
   plugins: [
     jigsaw({
       input: [
